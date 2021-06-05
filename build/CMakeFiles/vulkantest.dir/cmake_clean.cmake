3b3048bec88829c3776b0bc3e1489a8d28721e70
okiwi6 <ole.felber@gmail.com>
2021-06-05 13:27:05 +0200

    initial commit

---
+++ build/CMakeFiles/vulkantest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vulkantest.dir/src/main.cpp.o"
+  "CMakeFiles/vulkantest.dir/src/main.cpp.o.d"
+  "vulkantest"
+  "vulkantest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vulkantest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/vulkantest.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for vulkantest.
+# This may be replaced when dependencies are built.
