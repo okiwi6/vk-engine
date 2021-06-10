60589308702bf5996da838edf531cb428923b305
okiwi6 <ole.felber@gmail.com>
2021-06-10 16:18:18 +0200

    added command buffer allocation, frame drawing

--- src/first_app.cpp
+++ src/first_app.cpp
@@ -3,6 +3,8 @@
 
 // std
 #include <stdexcept>
+#include <vulkan/vulkan_core.h>
+#include <array>
 
 namespace vke {
     FirstApp::FirstApp() :
@@ -24,7 +26,10 @@ namespace vke {
     void FirstApp::run() {
         while (!vke_window.should_close()) {
             glfwPollEvents();
+            drawFrame();
         }
+
+        vkDeviceWaitIdle(vke_device.device());
     }
 
     void FirstApp::createPipelineLayout() {
@@ -51,7 +56,68 @@ namespace vke {
         );
     }
 
-    void FirstApp::createCommandBuffers() {}
+    void FirstApp::createCommandBuffers() {
+        // one command buffer per frame buffer
+        command_buffer.resize(vke_swap_chain.imageCount());
 
-    void FirstApp::drawFrame() {}
+        VkCommandBufferAllocateInfo alloc_info{};
+        alloc_info.sType  = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+        alloc_info.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+        alloc_info.commandPool = vke_device.getCommandPool();
+        alloc_info.commandBufferCount = static_cast<uint32_t>(command_buffer.size());
+
+        if (vkAllocateCommandBuffers(vke_device.device(), &alloc_info, command_buffer.data()) != VK_SUCCESS) {
+            throw std::runtime_error("failed to allocate command buffer");
+        }
+
+        for (int i = 0; i < command_buffer.size(); i++) {
+            VkCommandBufferBeginInfo begin_info{};
+            begin_info.sType  =VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+
+            if(vkBeginCommandBuffer(command_buffer[i], &begin_info) != VK_SUCCESS) {
+                throw std::runtime_error("failed to begin recording command buffer");
+            }
+
+            VkRenderPassBeginInfo render_pass_info{};
+            render_pass_info.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+            render_pass_info.renderPass = vke_swap_chain.getRenderPass();
+            render_pass_info.framebuffer = vke_swap_chain.getFrameBuffer(i);
+
+            render_pass_info.renderArea.offset = {0, 0};
+            render_pass_info.renderArea.extent = vke_swap_chain.getSwapChainExtent();
+
+            std::array<VkClearValue, 2> clear_values{};
+            clear_values[0].color = {0.1f, 0.1f, 0.1f, 1.0f};
+            clear_values[1].depthStencil = {1.0f, 0};
+
+            render_pass_info.clearValueCount = static_cast<uint32_t>(clear_values.size());
+            render_pass_info.pClearValues = clear_values.data();
+
+            vkCmdBeginRenderPass(command_buffer[i], &render_pass_info, VK_SUBPASS_CONTENTS_INLINE);
+            
+            vke_pipeline->bind(command_buffer[i]);
+            // 3 vertices, 1 instance (copies), (0, 0) no offset
+            vkCmdDraw(command_buffer[i], 3, 1, 0, 0);
+
+            vkCmdEndRenderPass(command_buffer[i]);
+            if(vkEndCommandBuffer(command_buffer[i]) != VK_SUCCESS) {
+                throw std::runtime_error("failed to record command buffer");
+            }
+        }   
+    }
+
+    void FirstApp::drawFrame() {
+        uint32_t image_index;
+        auto result = vke_swap_chain.acquireNextImage(&image_index);
+
+        if(result != VK_SUCCESS && result != VK_SUBOPTIMAL_KHR) {
+            throw std::runtime_error("failed to aquire swap chain image");
+        }
+
+        result = vke_swap_chain.submitCommandBuffers(&command_buffer[image_index], &image_index);
+
+        if(result != VK_SUCCESS) {
+            throw std::runtime_error("failed to present swap chain image");
+        }
+    }
 }
