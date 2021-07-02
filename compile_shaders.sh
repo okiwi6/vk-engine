d5482fd3ed3d20b866af7b70804f023cc4956bcd
okiwi6 <ole.felber@gmail.com>
2021-07-02 10:11:57 +0200

    shader compilation is now CMake target

--- compile_shaders.sh
+++ compile_shaders.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 GLSLC_PATH="/usr/bin/glslc"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 echo "using glslc at $GLSLC_PATH"
 
-$GLSLC_PATH shaders/simple_shader.vert -o shaders/simple_shader.vert.spv
-$GLSLC_PATH shaders/simple_shader.frag -o shaders/simple_shader.frag.spv
+
+$GLSLC_PATH "$SCRIPT_DIR/shaders/simple_shader.vert" -o "$SCRIPT_DIR/shaders/simple_shader.vert.spv"
+$GLSLC_PATH "$SCRIPT_DIR/shaders/simple_shader.frag" -o "$SCRIPT_DIR/shaders/simple_shader.frag.spv"
