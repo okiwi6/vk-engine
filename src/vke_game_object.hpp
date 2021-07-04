b646175a68ce5d9ab6765628164f944445073a44
okiwi6 <ole.felber@gmail.com>
2021-07-04 21:09:25 +0200

    improved mat4

--- src/vke_game_object.hpp
+++ src/vke_game_object.hpp
@@ -14,20 +14,50 @@
             glm::vec3 scale{1.0f, 1.0f, 1.0f};
             glm::vec3 rotation{};
 
-            glm::mat4 mat4() {
-                // Identity * Translate
-                auto transform = glm::translate(glm::mat4{1.f}, translation);
+            // glm::mat4 mat4() {
+            //     // Identity * Translate
+            //     auto transform = glm::translate(glm::mat4{1.f}, translation);
 
-                // * Rotation (Y1, X2, Z3)
-                transform = glm::rotate(transform, rotation.y, {0.f, 1.f, 0.f});
-                transform = glm::rotate(transform, rotation.x, {1.f, 0.f, 0.f});
-                transform = glm::rotate(transform, rotation.z, {0.f, 0.f, 1.f});
+            //     // * Rotation (Y1, X2, Z3)
+            //     transform = glm::rotate(transform, rotation.y, {0.f, 1.f, 0.f});
+            //     transform = glm::rotate(transform, rotation.x, {1.f, 0.f, 0.f});
+            //     transform = glm::rotate(transform, rotation.z, {0.f, 0.f, 1.f});
                 
-                // * Scale
-                transform = glm::scale(transform, scale);
+            //     // * Scale
+            //     transform = glm::scale(transform, scale);
 
-                return transform;
-            };
+            //     return transform;
+            // };
+
+            glm::mat4 mat4() {
+                // faster implementation because there is no matrix multiplication anymore
+                const float c3 = glm::cos(rotation.z);
+                const float s3 = glm::sin(rotation.z);
+                const float c2 = glm::cos(rotation.x);
+                const float s2 = glm::sin(rotation.x);
+                const float c1 = glm::cos(rotation.y);
+                const float s1 = glm::sin(rotation.y);
+                return glm::mat4{
+                    {
+                        scale.x * (c1 * c3 + s1 * s2 * s3),
+                        scale.x * (c2 * s3),
+                        scale.x * (c1 * s2 * s3 - c3 * s1),
+                        0.0f,
+                    },
+                    {
+                        scale.y * (c3 * s1 * s2 - c1 * s3),
+                        scale.y * (c2 * c3),
+                        scale.y * (c1 * c3 * s2 + s1 * s3),
+                        0.0f,
+                    },
+                    {
+                        scale.z * (c2 * s1),
+                        scale.z * (-s2),
+                        scale.z * (c1 * c2),
+                        0.0f,
+                    },
+                    {translation.x, translation.y, translation.z, 1.0f}};
+                        }
         };
 
         class VkeGameObject {
